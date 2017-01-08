fd52e2c53704a44c2a556721ce95aff4b27abb59
Clive Chan <cc@clive.io>
2017-01-08 04:32:21 -0500

    use slice not substr, also allow dash in subdomain

--- bin/https.js
+++ bin/https.js
@@ -3,7 +3,7 @@
 let request = require('request-promise');
 
 function addSubdomain(subdomain){
-  if(/[^a-zA-Z0-9]/.test(subdomain))
+  if(/[^a-zA-Z0-9\-]/.test(subdomain))
     return Promise.reject("Non-alpha-numeric-dash subdomain name.");
   else
     return request('http://localhost:10000/https/' + subdomain);
